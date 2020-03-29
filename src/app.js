c9e8af3c033652ee0f00fe62d98db4ceb954eb8a
Lucas Sierota <lucasemanuelss@hotmail.com>
2020-03-29 02:22:43 -0300

    Backend para deploy be-the-hero

---
+++ src/app.js
@@ -0,0 +1,19 @@
+require('dotenv');
+
+const express = require('express');
+const routes  = require('./routes');
+const cors    = require('cors');
+const { errors } = require('celebrate');
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.REACT_APP_URL
+}));
+app.use(express.json());
+app.use(routes);
+
+app.use(errors());
+
+module.exports = app;
+
